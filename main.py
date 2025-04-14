f575293f17ee7b0096254ae83d7716c419e60df1
AtQoom <eyes@naver.com>
2025-04-14 11:18:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -100,15 +100,6 @@ def place_order(direction, step):
         send_telegram_message(f"[에러] 주문 실패: {str(e)}")
 
 
-
-    # ✅ 여기 추가!
-    print(f"[Bitget 응답] 상태코드: {res.status_code}")
-    print(f"[Bitget 응답 본문] {res.text}")
-
-    # 알림 (나중에 수정해도 됨)
-    send_telegram_message(f"[진입] {direction} {step}단계 주문 응답: {res.text}")
-
-
 # ✅ 청산
 def close_position(direction, reason):
     side = closeSide.get(direction)
